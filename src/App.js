76a8b2964d06d505e4096b94179084dec16bbb86
Bhil-Hitesh <bhiluh819@gmail.com>
2022-09-22 12:35:03 +0530

    react_final_pro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Product from './Containers/Product';
+import Arrivals from './Containers/Arrivals';
+import Collection from './Containers/Collection';
+import Feature from './Containers/Feature';
+import Blog from './Containers/Blog';
+import Clients from './Containers/Clients';
+import Newsletter from './Containers/Newsletter';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Product/>
+      <Arrivals/>
+      <Collection/>
+      <Feature/>
+      <Blog/>
+      <Clients/>
+      <Newsletter/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/Containers/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const auth=()=>{
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default Login;
